9d421de18aa9ace21439fb8f8f26c92bea42bf65
Taylor Seamans <taylor.seamans@yahoo.com>
2023-09-15 11:15:14 -0700

    Extend theme to include color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx, html}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: "#E31C5F",
+        "brand-dark": "#D70466",
+        background: "#FFFFFF",
+        border: {
+          strong: "#222222",
+          default: "#A0A0A0",
+          weak: "#CCCCCC",
+        },
+        text: {
+          default: "#222222",
+          weak: "#717171",
+        },
+      },
+    },
   },
   plugins: [],
 };
