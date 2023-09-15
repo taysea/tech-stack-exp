99c82fe0b536bc9f8e5bdb07534c5af78757bd1b
Taylor Seamans <taylor.seamans@yahoo.com>
2023-09-15 13:12:48 -0700

    First pass Heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,36 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
-import { Button } from "./components";
+import { Button, Heading } from "./components";
+
+const Container = ({ ...rest }) => (
+  <div
+    className="border-solid border border-border-weak p-6 rounded-xl flex flex-col gap-4 items-start"
+    {...rest}
+  />
+);
+
+const Badge = ({ ...rest }) => (
+  <div
+    className="bg-border-weak/30 font-bold border-solid border border-border-weak/50 px-3 py-1 rounded-md flex flex-col gap-4 items-start"
+    {...rest}
+  />
+);
 
 function App() {
   return (
-    <div className="p-12">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <div className="border-solid border border-border-weak p-6 rounded-xl flex flex-col gap-4 items-start">
-        <h1 className="text-3xl text-text-default font-bold">Hello world!</h1>
-        <div>
-          <p className="text-text-default">
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="text-text-default">
-          Click on the Vite and React logos to learn more
-        </p>
+    <div className="p-12 flex flex-col items-start gap-4">
+      <Heading>Component sticker sheet</Heading>
+      <Container>
+        <Badge>Heading</Badge>
+        {[1, 2, 3, 4, 5, 6].map((level) => (
+          <Heading level={level}>{`Heading ${level}`}</Heading>
+        ))}
+      </Container>
+      <Container>
+        <Badge>Button</Badge>
+
         <Button label="Click me" onClick={() => console.log("clicked")} />
-      </div>
+      </Container>
     </div>
   );
 }
